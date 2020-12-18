4b50666f362f4b835d520eda23a9208ee299b21d
Lucas Pompeu <lucaspompeuneves@gmail.com>
2020-12-18 14:05:33 -0300

    Aula 03

--- 01-ByteBank/Program.cs
+++ 01-ByteBank/Program.cs
@@ -10,7 +10,20 @@ namespace _01_ByteBank
     {
         static void Main(string[] args)
         {
-            new ContaCorrente();
+            ContaCorrente contaDaGabriela = new ContaCorrente();
+
+            contaDaGabriela.titular = "Gabriela";
+            contaDaGabriela.agencia = 863;
+            contaDaGabriela.numero = 863452;
+            contaDaGabriela.saldo = 100;
+
+            Console.WriteLine(contaDaGabriela.titular);
+            Console.WriteLine("Agencia: " + contaDaGabriela.agencia);
+            Console.WriteLine("Número: " + contaDaGabriela.numero);
+            Console.WriteLine("Saldo: " + contaDaGabriela.saldo);
+
+            contaDaGabriela.saldo += 200;
+            Console.WriteLine("Novo saldo: " + contaDaGabriela.saldo);
 
             Console.ReadLine();
         }
