f7865865aec5baa48f1a9ce5e494802d01b6880a
Lucas Pompeu <lucaspompeuneves@gmail.com>
2020-12-31 11:31:59 -0300

    Curso finalizado

---
+++ 05-ByteBank/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _05_ByteBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Cliente gabriela = new Cliente();
+
+            //gabriela.nome = "Gabriela";
+            //gabriela.profissao = "Desenvolvedora C#";
+            //gabriela.cpf = "454.545.646-20";
+
+            ContaCorrente conta = new ContaCorrente();
+
+            //conta.titular = gabriela;
+            //conta.titular = new Cliente();
+            //conta.titular.nome = "Gabriela Costa";
+            //conta.titular.cpf = "454.545.646-20";
+            //conta.titular.profissao = "Desenvolvedora C#";
+
+            conta.saldo = 500;
+            conta.agencia = 563;
+            conta.numero = 563472;
+
+            if (conta.titular == null)
+            {
+                Console.WriteLine("Ops, a referância em conta.titular é null");
+            }
+
+            //Console.WriteLine(gabriela.nome);
+            Console.WriteLine(conta.titular);
+            //Console.WriteLine(conta.titular.nome);
+            //Console.WriteLine(conta.titular.cpf);
+            //Console.WriteLine(conta.titular.profissao);
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ 06-ByteBank/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _06_ByteBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaCorrente conta = new ContaCorrente();
+            Cliente cliente = new Cliente();
+
+            cliente.Nome = "Guilherme";
+            cliente.CPF = "112.569.985-56";
+            cliente.Profissao = "Desenvolvedor";
+
+            conta.Saldo = 10;
+            conta.Titular = cliente;
+
+            Console.WriteLine(conta.Saldo);
+            Console.WriteLine(conta.Titular.Nome);
+
+            Console.ReadLine();
+        }
+    }
+}
